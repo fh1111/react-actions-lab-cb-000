a9dc1aa15f557798cb4762bf16d9efa6f772d308
Fer Hernandez <fh.1111@gmail.com>
2019-03-03 19:55:01 +0000

    Automatically backed up by Learn

--- components/Spreadsheet.js
+++ components/Spreadsheet.js
@@ -11,8 +11,15 @@ export default class Spreadsheet extends React.Component {
       focused: null,
       table: [
         ['']
-      ]      
+      ]
     };
+    this.handleAddColumn = actions.addColumn.bind(this);
+    this.handleAddRow = actions.addRow.bind(this);
+    this.handleRemoveColumn = actions.removeColumn.bind(this);
+    this.handleRemoveRow = actions.removeRow.bind(this);
+    this.handleChange = actions.changeCell.bind(this);
+    this.handleFocus = actions.focusCell.bind(this);
+    this.handleBlur = actions.blurCell.bind(this);    
 
   }
 
